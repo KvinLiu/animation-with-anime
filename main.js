1b0ea942b84053a5698d57d564b790749af73a36
Kevin Liu <kevin07liu@gmail.com>
2017-09-07 17:49:26 +0800

    Add play and pause functionalities

--- main.js
+++ main.js
@@ -50,7 +50,7 @@
 // 	}
 // });
 
-anime({
+var playPause = anime({
 	targets: 'div.box',
 	translateY: [
 		{ value: 200, duration: 500 },
@@ -62,7 +62,11 @@ anime({
 	},
 	delay: function(el, i, l) {
 		return i * 1000;
-	}
-
+	},
+	autoplay: false,
+	loop: true
 });
+
+document.querySelector('#boxes .play').onclick = playPause.play;
+document.querySelector('#boxes .pause').onclick = playPause.pause;
 
