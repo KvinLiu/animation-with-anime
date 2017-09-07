07ede81cffa4e3d08290b22df5be3fb7d6d18378
Kevin Liu <kevin07liu@gmail.com>
2017-09-07 12:18:50 +0800

    Add animation to .blue and .red box

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 anime({
+	targets: 'div.red',
+	translateY: [
+		{ value: 100, duration: 500},
+		{ value: 0, duration: 800}
+	],
+	rotate: {
+		value: '1turn',
+		easing: 'easeInOutSine'
+	}
+});
 
+anime({
+	targets: 'div.blue',
+	translateY: [
+		{ value: 100, duration: 500, delay: 1000},
+		{ value: 0, duration: 800}
+	],
+	rotate: {
+		value: '1turn',
+		easing: 'easeInOutSine',
+		delay: 1000
+	}
 });
